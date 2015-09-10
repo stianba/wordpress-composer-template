ed8f3df45493b747651831dcc1a21d3eed2dd6b0
Stian Bakkane <stianba@gmail.com>
2015-09-10 14:18:52 +0200

    First commit

---
+++ web/app/themes/template-theme/index.php
@@ -0,0 +1,10 @@
+<?php use PackagistExample\Doolah; ?>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+</head>
+<body>
+  <h1><?php echo Doolah::talk('PACKAGIST'); ?></h1>
+</body>
+</html>
